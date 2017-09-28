f20c392efce3147eeaab69af4ef67ca9d0b0d157
tanem <tane.morgan@gmail.com>
2017-09-29 06:35:18 +1300

    Update deps

--- .jest-setup.js
+++ .jest-setup.js
@@ -1,3 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 if (!global.SVGSVGElement && global.HTMLUnknownElement) {
-  global.SVGSVGElement = global.HTMLUnknownElement
+  global.SVGSVGElement = global.HTMLUnknownElement;
 }
--- bin/makeWebpackConfig.js
+++ bin/makeWebpackConfig.js
@@ -22,7 +22,7 @@ export default function makeWebpackConfig(buildType) {
       loaders: [
         {
           test: /\.js$/,
-          loader: 'babel',
+          loader: 'babel-loader',
           exclude: /node_modules/
         }
       ]
