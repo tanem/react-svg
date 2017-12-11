7f2579779413285b581bc8f226694ddf50971e21
tanem <tane.morgan@gmail.com>
2017-12-12 09:04:35 +1300

    Tidy up test setup

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,7 @@
-import { configure } from 'enzyme';
+import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 if (!global.SVGSVGElement && global.HTMLUnknownElement) {
   global.SVGSVGElement = global.HTMLUnknownElement;
