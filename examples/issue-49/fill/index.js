de67750edd482f5eefbbf4e5f1402cc8f68c23fc
tanem <tane.morgan@gmail.com>
2018-06-15 21:13:58 +1200

    Remove webpack

--- examples/issue-49/fill/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ReactSVG from '../../../src'
-
-ReactDOM.render(
-  <ReactSVG path="atomic.svg" svgClassName="svg-class-name" />,
-  document.querySelector('.Root')
-)
