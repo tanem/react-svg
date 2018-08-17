b64030e6ca4783fdf2afbf24fa0395541d4ae256
Tane Morgan <tane.morgan@gmail.com>
2018-08-18 09:19:02 +1200

    Fix typescript example

--- examples/typescript/src/index.tsx
+++ examples/typescript/src/index.tsx
@@ -1,1 +1,5 @@
-import './App'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import ReactSVG from 'react-svg'
+
+ReactDOM.render(<ReactSVG path="svg.svg" />, document.getElementById('root'))
