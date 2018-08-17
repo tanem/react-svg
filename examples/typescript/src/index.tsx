c3653c078ab65bf87e58beabb09560e777794def
Tane Morgan <tane.morgan@gmail.com>
2018-08-18 09:04:00 +1200

    Add keywords to examples

--- examples/typescript/src/index.tsx
+++ examples/typescript/src/index.tsx
@@ -1,5 +1,1 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import ReactSVG from 'react-svg'
-
-ReactDOM.render(<ReactSVG path="svg.svg" />, document.getElementById('root'))
+import './App'
