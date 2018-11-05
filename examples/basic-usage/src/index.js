9345d99e6234a7c65220d67e0e6f14cedfd405b8
Tane Morgan <tane.morgan@gmail.com>
2018-11-06 06:53:26 +1300

    Decruft basic example

--- examples/basic-usage/src/index.js
+++ examples/basic-usage/src/index.js
@@ -2,7 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactSVG from 'react-svg'
 
-ReactDOM.render(
-  <ReactSVG fallback={<span>Error!</span>} src="svg.svg" />,
-  document.getElementById('root')
-)
+ReactDOM.render(<ReactSVG src="svg.svg" />, document.getElementById('root'))
