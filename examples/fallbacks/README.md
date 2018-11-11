84d50877f30b8c433978a7abf5d4be8aaa5359d8
Tane Morgan <tane.morgan@gmail.com>
2018-11-12 05:32:13 +1300

    Add more fallback examples

--- examples/fallbacks/README.md
+++ examples/fallbacks/README.md
@@ -1,4 +1,4 @@
-# ReactSVG Fallback Example
+# ReactSVG Fallbacks Example
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
---
+++ examples/fallbacks/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactSVG from 'react-svg'
+
+class ComponentFallback extends React.Component {
+  render() {
+    return <img alt="nyan" src="nyan.jpg" />
+  }
+}
+const FunctionFallback = () => <img alt="doge" src="doge.png" />
+const StringFallback = 'div'
+
+ReactDOM.render(
+  <React.Fragment>
+    <ReactSVG fallback={ComponentFallback} src="notfound.svg" />
+    <ReactSVG fallback={FunctionFallback} src="notfound.svg" />
+    <ReactSVG fallback={StringFallback} src="notfound.svg" />
+  </React.Fragment>,
+  document.getElementById('root')
+)
