b662088c99b5c22aee8334895b6a30d34cc7afba
Tane Morgan <tane.morgan@gmail.com>
2018-11-25 12:24:10 +1300

    Add example for older TypeScript version

---
+++ examples/typescript-latest/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import ReactSVG from 'react-svg'
+
+ReactDOM.render(<ReactSVG src="svg.svg" />, document.getElementById('root'))
