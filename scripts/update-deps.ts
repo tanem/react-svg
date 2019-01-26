40fcbf7957789f24361fa70c2166046f81dd515e
Tane Morgan <tane.morgan@gmail.com>
2019-01-27 12:56:53 +1300

    Convert updatedeps script to TS

--- scripts/update-deps.ts
+++ scripts/update-deps.ts
@@ -1,18 +1,20 @@
+// tslint:disable no-var-requires
+
 const glob = require('glob')
 const shell = require('shelljs')
 
 glob
   .sync('examples/*/package.json')
   .concat('package.json')
-  .forEach(packageFile => {
+  .forEach((packageFile: string) => {
     shell.exec(`ncu -a --packageFile ${packageFile}`)
   })
 
 if (
   shell
     .exec('git ls-files --exclude-standard --modified --others')
-    .split('\n')
-    .some(f => f === 'package.json')
+    .stdout.split('\n')
+    .some((f: string) => f === 'package.json')
 ) {
   shell.exec('npm i')
 }
