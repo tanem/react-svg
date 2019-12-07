a3418c0a223e83dbacd066cc0157b091c5639594
Tane Morgan <464864+tanem@users.noreply.github.com>
2019-12-08 11:01:58 +1300

    Remove typescript 2.x example

--- examples/typescript-latest/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { ReactSVG } from 'react-svg'
-
-ReactDOM.render(<ReactSVG src="svg.svg" />, document.getElementById('root'))
