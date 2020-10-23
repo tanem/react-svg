a72bff1b4bfc873104fd48e45553708ac3229d26
Tane Morgan <464864+tanem@users.noreply.github.com>
2020-10-24 08:33:11 +1300

    Add ability to bypass request cache

--- examples/api-usage/src/index.js
+++ examples/api-usage/src/index.js
@@ -20,6 +20,7 @@ ReactDOM.render(
     fallback={() => <span>Error!</span>}
     loading={() => <span>Loading</span>}
     renumerateIRIElements={false}
+    useRequestCache={false}
     wrapper="span"
     className="wrapper-class-name"
     onClick={() => {
