5680a9070d6930bf35a0c46ebbd1cd0060e01530
Tane Morgan <464864+tanem@users.noreply.github.com>
2022-05-01 13:20:34 +1200

    Drop react 15 support, add more react version tests

--- scripts/test-react.ts
+++ scripts/test-react.ts
@@ -4,16 +4,17 @@ import shell from 'shelljs'
 shell
   .ls('-d', path.join(process.cwd(), 'test', 'react', '*'))
   .forEach((reactDir) => {
-    shell.exec('npm i --no-package-lock', {
+    const reactVersion = path.basename(reactDir)
+    shell.echo(`Starting React ${reactVersion} tests`)
+    shell.exec('npm i --no-package-lock --quiet --no-progress', {
       cwd: reactDir,
     })
     ;['cjs', 'cjsprod', 'es', 'src', 'umd', 'umdprod'].forEach((testType) => {
       const { code } = shell.exec(
-        `REACT_VERSION=${path.basename(
-          reactDir
-        )} npx jest --config ./scripts/jest/config.${testType}.js --coverage false`,
+        `REACT_VERSION=${reactVersion} npx jest --config ./scripts/jest/config.${testType}.js --coverage false`,
         { fatal: true }
       )
+      shell.echo(`Finished testing React ${reactVersion}`)
       if (code !== 0) {
         shell.exit(code)
       }
